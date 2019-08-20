037f3104af1f2768c95abc57ce902b5102800309
Oleksandr Palieiev <oleksandr.palieiev@nearshorepartner.ch>
2019-08-20 15:10:23 +0300

    add solution task - section 7

--- s1/section 7-ES6/script.js
+++ s1/section 7-ES6/script.js
@@ -654,7 +654,7 @@ const allStreets = [new Street('Ocean Avenue', 1999, 1.1, 4), new Street('Evergr
 
 function calc() {
 
-    const sum = arr.reduce((prev, cur, index) => prev + current, 0);
+    const sum = arr.reduce((prev, cur, index) => prev + cur, 0);
 
      retrun [sum, sum / arr.length];
 }
@@ -667,14 +667,28 @@ function reportParks(p) {
     p.forEach(el => el.treeDensity());
 
     // средний возраст
-    const ages = p.map(el => 2016 - el.buildYear);
+    const ages = p.map(el => new Date().getFullYear() - el.buildYear);
     const [totalAge, avgAge] = calc(ages);
+    console.log(`Our ${p.length} parcs have an average of ${avgAge} years.`);
 
     // в каком парке больше 1000 деревьев
+    const i = p.map(el => el.numTrees).findIndex(el => el >= 1000);
+    console.log(`${p[i].name} has more than 1000 trees.`);
 
 }
 
 function reportStreets(s) {
+
+    console.log('-----STREETS REPORT-----');
+
+    // общая длинна городских улиц
+    const [totalLength, avgLength] = calc(s.map(el => el.length));
+    console.log(`Our ${s.length} streets have a total length of ${totalLength} 
+    km, with an average of ${avgLength} km.`);
+    
+
+    // класификация рызмеров
+    s.forEach(el => el.classifyStreet());
 
 }
 
